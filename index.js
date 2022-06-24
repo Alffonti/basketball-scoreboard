ac88f78998492e430cf05e4b9d58caea36a6b996
Alf Fonti <alffonti@gmail.com>
2022-06-24 15:36:29 -0300

    update quotes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const homeScoreEl = document.getElementById('home-score')
 const guestScoreEl = document.getElementById('guest-score')
-const homeScoreBtnOne = document.getElementById("home-score-btn-1")
-const guestScoreBtnOne = document.getElementById("guest-score-btn-1")
-const homeScoreBtnTwo = document.getElementById("home-score-btn-2")
-const guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
-const homeScoreBtnThree = document.getElementById("home-score-btn-3")
-const guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+const homeScoreBtnOne = document.getElementById('home-score-btn-1')
+const guestScoreBtnOne = document.getElementById('guest-score-btn-1')
+const homeScoreBtnTwo = document.getElementById('home-score-btn-2')
+const guestScoreBtnTwo = document.getElementById('guest-score-btn-2')
+const homeScoreBtnThree = document.getElementById('home-score-btn-3')
+const guestScoreBtnThree = document.getElementById('guest-score-btn-3')
 
 let homeScore = 0
 let guestScore = 0
