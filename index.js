33bf5cfcc0402327b48f93e0e34f211504defb22
Alf Fonti <alffonti@gmail.com>
2022-10-20 18:07:04 -0500

    Added onclick events in index.html

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const homeScoreEl = document.getElementById('home-score')
 const guestScoreEl = document.getElementById('guest-score')
-const homeScoreBtnOne = document.getElementById('home-score-btn-1')
-const guestScoreBtnOne = document.getElementById('guest-score-btn-1')
-const homeScoreBtnTwo = document.getElementById('home-score-btn-2')
-const guestScoreBtnTwo = document.getElementById('guest-score-btn-2')
-const homeScoreBtnThree = document.getElementById('home-score-btn-3')
-const guestScoreBtnThree = document.getElementById('guest-score-btn-3')
-const newGameBtn = document.getElementById('new-game-btn')
 
 let homeScore = 0
 let guestScore = 0
+
+function increment(points, team) {
+    if (team === 'home') {
+        homeScore += points;
+        homeScoreEl.textContent = homeScore;
+    } else {
+        guestScore += points;
+        guestScoreEl.textContent = guestScore;
+    }
+    winningTeam()
+}
 
 function winningTeam() {
     if (homeScore > guestScore) {
@@ -18,53 +22,17 @@ function winningTeam() {
     } else if (guestScore > homeScore) {
         guestScoreEl.style.color = '#4ff968' // green
         homeScoreEl.style.color = '#F94F6D'
+    } else {
+        guestScoreEl.style.color = '#F94F6D' 
+        homeScoreEl.style.color = '#F94F6D'
     }
 }
 
-homeScoreBtnOne.addEventListener('click', function increaseHomeScoreOne(){
-    // console.log('clicked');
-    homeScore++
-    // console.log(homeScore)
-    homeScoreEl.textContent = homeScore
-    winningTeam()
-})
-
-homeScoreBtnTwo.addEventListener('click', function increaseHomeScoreTwo(){
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-    winningTeam()
-})
-
-homeScoreBtnThree.addEventListener('click', function increaseHomeScoreThree(){
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-    winningTeam()
-})
-
-guestScoreBtnOne.addEventListener('click', function increaseguestScoreOne(){
-    guestScore++
-    // console.log(guestScore)
-    guestScoreEl.textContent = guestScore
-    winningTeam()
-})
-
-guestScoreBtnTwo.addEventListener('click', function increaseguestScoreTwo(){
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
-    winningTeam()
-})
-
-guestScoreBtnThree.addEventListener('click', function increaseguestScoreThree(){
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
-    winningTeam()
-})
-
-newGameBtn.addEventListener('click', function newGame(){
+function newGame() {
     homeScore = 0;
     guestScore = 0;
     homeScoreEl.textContent = 0;
     guestScoreEl.textContent = 0;
-    homeScoreEl.style.color = '#F94F6D';
-    guestScoreEl.style.color = '#F94F6D';
-})
+    guestScoreEl.style.color = '#F94F6D' 
+    homeScoreEl.style.color = '#F94F6D'
+}
